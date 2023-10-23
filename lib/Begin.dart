765c42a8703a9d78ab6017863d9a9b6adc736c5d
chill2305 <125633797+chill2305@users.noreply.github.com>
2023-10-23 22:09:43 +0700

    Hoàng Triều branch

--- lib/Begin.dart
+++ lib/Begin.dart
@@ -10,6 +10,6 @@ class BeginPage extends StatefulWidget {
 class _BeginPageState extends State<BeginPage> {
   @override
   Widget build(BuildContext context) {
-    return Text("Của Hoàng Triều");
+    return Text("Test fetch");
   }
 }
